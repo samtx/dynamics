466842cd99a4473f5112c426a1265a80e95e2ec8
Sam Friedman <samtx@outlook.com>
2017-10-24 11:45:45 -0500

    add extra loop to compute for both 377 and  754 rpms

--- hw2.m
+++ hw2.m
@@ -5,84 +5,96 @@
 
 function [] = hw2()
 
+% Constants for length of piston arms
 L1 = 0.241;  % [m]
 L2 = 1.206;  % [m]
+
 % Solve the kinematics of the system, assuming a constant theta_dt
-theta_dt = 39.5;  % [rad/s], constant angular velocity
-dt = 0.001;         % [s],  time step
-n = ceil(2*pi/(theta_dt*dt)) ; % total time steps, rounded up to nearest integer
-t = [0:dt:(n-1)*dt]'; %#ok<NBRAK>
-theta = t*theta_dt;  % theta vector
-% initialize state vectors
-phi = zeros(n,1);
-phi_dt = zeros(n,1);
-phi_dt2 = zeros(n,1);
-xp = zeros(n,1);
-xp_dt = zeros(n,1);
-xp_dt2 = zeros(n,1);
+theta_dt_list = [39.5, 79.0];      % angular velocity in rad/s
+rpm_list = {'377rpm','754rpm'};    % string of RPM values for saving data
 
-for i = 1:n
-    phi(i) = asin(L1/L2*sin(theta(i)));
-    phi_dt(i) = theta_dt*(L1*cos(theta(i)))/(L2*cos(phi(i)));
-    phi_dt2(i) = 1/(L2*cos(phi(i)))*(L2*sin(phi(i))*phi_dt(i)^2-L1*sin(theta(i))*theta_dt^2);
-    xp(i) = L1*cos(theta(i))+L2*cos(phi(i));
-    xp_dt(i) = -L1*sin(theta(i))*theta_dt-L2*sin(phi(i))*phi_dt(i);
-    xp_dt2(i) = -L1*cos(theta(i))*theta_dt^2-L2*(cos(phi(i))*phi_dt(i)^2+sin(phi(i))*phi_dt2(i));
+% Loop over each given constant value of theta_dt
+for j = 1:length(theta_dt_list)
+    theta_dt = theta_dt_list(j);   % [rad/s], constant angular velocity
+    dt = 0.001;                    % [s],  time step
+    n = ceil(2*pi/(theta_dt*dt)) ; % total time steps for one revolution, 
+                                   % rounded up to nearest integer
+    t = [0:dt:(n-1)*dt]'; %#ok<NBRAK>
+    theta = t*theta_dt;  % vector of theta values
+    
+    % initialize state vectors
+    phi = zeros(n,1);
+    phi_dt = zeros(n,1);
+    phi_dt2 = zeros(n,1);
+    xp = zeros(n,1);
+    xp_dt = zeros(n,1);
+    xp_dt2 = zeros(n,1);
+    
+    % Compute values for phi, phi_dt, phi_dt2, xp, xp_dt, xp_dt2 for each
+    % value of theta
+    for i = 1:n
+        phi(i) = asin(L1/L2*sin(theta(i)));
+        phi_dt(i) = theta_dt*(L1*cos(theta(i)))/(L2*cos(phi(i)));
+        phi_dt2(i) = 1/(L2*cos(phi(i)))*(L2*sin(phi(i))*phi_dt(i)^2-L1*sin(theta(i))*theta_dt^2);
+        xp(i) = L1*cos(theta(i))+L2*cos(phi(i));
+        xp_dt(i) = -L1*sin(theta(i))*theta_dt-L2*sin(phi(i))*phi_dt(i);
+        xp_dt2(i) = -L1*cos(theta(i))*theta_dt^2-L2*(cos(phi(i))*phi_dt(i)^2+sin(phi(i))*phi_dt2(i));
+    end
+    
+    m1 = 908;    % [kg]     mass of arm AB
+    m2 = 245;    % [kg]     mass of arm BC
+    mp = 590;    % [kg]     mass of piston
+    g = 9.81;    % [m/s^2]  gravitational constant
+    w1 = m1*g;   % [N]      weight of arm AB
+    w2 = m2*g;   % [N]      weight of arm BC
+    I2g = m2*L2^2/12; % [kg.m^2] moment of inertia
+    M = zeros(n,1);   % [N.m]    applied moment
+    Bx = zeros(n,1);  % [N]      reaction force at location B
+    By = zeros(n,1);
+    Cx = zeros(n,1);  % [N]      reaction force at location C
+    Cy = zeros(n,1);
+    
+    for i = 1:n
+        A = [
+            1,    L1*sin(theta(i)),   -L1*cos(theta(i)),                   0,                  0;
+            0, -0.5*L2*sin(phi(i)), -0.5*L2*cos(phi(i)), -0.5*L2*sin(phi(i)), 0.5*L2*cos(phi(i));
+            0,                  -1,                   0,                   1,                  0;
+            0,                   0,                  -1,                   0,                 -1;
+            0,                   0,                   0,                  -1,                  0;
+            ];
+        
+        b = [
+            w1*0.5*L1*cos(theta(i));
+            -I2g*phi_dt2(i);
+            m2*L1*theta_dt^2*cos(theta(i))+m2*0.5*L2*cos(phi(i))*phi_dt(i)^2+m2*0.5*L2*sin(phi(i))*phi_dt2(i);
+            m1*0.5*L1*theta_dt^2*sin(theta(i))-w2;
+            mp*L1*theta_dt^2*cos(theta(i))+mp*L2*phi_dt(i)^2*cos(phi(i))+mp*L2*sin(phi(i))*phi_dt2(i);
+            ];
+        
+        x = A\b;  % solve matrix equation
+        M(i) = x(1); % store data values
+        Bx(i) = x(2);
+        By(i) = x(3);
+        Cx(i) = x(4);
+        Cy(i) = x(5);
+    end
+    
+    % Compute Ax, Ay reaction forces
+    Ax = -Bx;
+    Ay = w1 + By;
+    
+    % Compute net reaction forces
+    Anet = sqrt(Ax.^2+Ay.^2); %#ok<NASGU>
+    Bnet = sqrt(Bx.^2+By.^2); %#ok<NASGU>
+    Cnet = sqrt(Cx.^2+Cy.^2); %#ok<NASGU>
+    
+    % Save data to .dat files
+    vars = {'theta','phi','phi_dt','phi_dt2','xp','xp_dt','xp_dt2','M','Bx','By','Cx','Cy','Ax','Ay','Anet','Bnet','Cnet'};
+    for i = 1:length(vars)  % iterate through variables to save data
+        tbl = array2table([t, eval(vars{i})]);
+        writetable(tbl,['data/',rpm_list{j},'_',vars{i},'.dat'],'WriteVariableNames',false,'Delimiter',' ');
+    end
 end
-
-m1 = 908;    % [kg]
-m2 = 245;    % [kg]
-mp = 590;
-g = 9.81;    % [m/s^2]  gravitational constant
-w1 = m1*g; % [N]
-w2 = m2*g; % [N]
-I2g = m2*L2^2/12; % [m.o.i.]
-M = zeros(n,1); % applied moment
-Bx = zeros(n,1);  % reaction forces
-By = zeros(n,1);
-Cx = zeros(n,1);
-Cy = zeros(n,1);
-
-for i = 1:n
-    A = [
-    1,    L1*sin(theta(i)),   -L1*cos(theta(i)),                   0,                  0;
-    0, -0.5*L2*sin(phi(i)), -0.5*L2*cos(phi(i)), -0.5*L2*sin(phi(i)), 0.5*L2*cos(phi(i));
-    0,                  -1,                   0,                   1,                  0;    
-    0,                   0,                  -1,                   0,                 -1;
-    0,                   0,                   0,                  -1,                  0;
-    ];
-
-    b = [
-    w1*0.5*L1*cos(theta(i));
-    -I2g*phi_dt2(i);
-    m2*L1*theta_dt^2*cos(theta(i))+m2*0.5*L2*cos(phi(i))*phi_dt(i)^2+m2*0.5*L2*sin(phi(i))*phi_dt2(i);
-    m1*0.5*L1*theta_dt^2*sin(theta(i))-w2;
-    mp*L1*theta_dt^2*cos(theta(i))+mp*L2*phi_dt(i)^2*cos(phi(i))+mp*L2*sin(phi(i))*phi_dt2(i);
-    ];
-
-    x = A\b;  % solve matrix equation
-    M(i) = x(1); % store data values
-    Bx(i) = x(2);
-    By(i) = x(3);
-    Cx(i) = x(4);
-    Cy(i) = x(5);    
-end
-
-% Compute Ax, Ay reaction forces
-Ax = -Bx; 
-Ay = w1 + By; 
-Anet = sqrt(Ax.^2+Ay.^2); %#ok<NASGU>
-
-Bnet = sqrt(Bx.^2+By.^2); %#ok<NASGU>
-Cnet = sqrt(Cx.^2+Cy.^2); %#ok<NASGU>
-
-% Save data to .dat files
-vars = {'theta','phi','phi_dt','phi_dt2','xp','xp_dt','xp_dt2','M','Bx','By','Cx','Cy','Ax','Ay','Anet','Bnet','Cnet'};
-for i = 1:length(vars)  % iterate through variables to save data
-	tbl = array2table([t, eval(vars{i})]);
-    writetable(tbl,['data/',vars{i},'.dat'],'WriteVariableNames',false,'Delimiter',' ');
-end
-
 % skip plotting for now...
 return
 
